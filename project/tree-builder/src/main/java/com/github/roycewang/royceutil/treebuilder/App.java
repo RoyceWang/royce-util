c7a77dcf21a47455a9d5fde558886b4b39937bfe
RoyceWang <carefree0@qq.com>
2019-04-26 17:27:48 +0800

    init project tree-builder

---
+++ project/tree-builder/src/main/java/com/github/roycewang/royceutil/treebuilder/App.java
@@ -0,0 +1,10 @@
+package com.github.roycewang.royceutil.treebuilder;
+
+/**
+ * Hello world!
+ */
+public class App{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
